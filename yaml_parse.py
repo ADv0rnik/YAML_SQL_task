5888a1b814b9884658ae7f02d405ab968875d942
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-11-14 21:11:49 +0300

    updated

--- yaml_parse.py
+++ yaml_parse.py
@@ -1,12 +1,9 @@
 import yaml
 
-with open('games.yaml', 'r') as stream:
+with open('a.yaml', 'r') as stream:
     data_loaded = yaml.load(stream, Loader=yaml.SafeLoader)
 
-    template = 'INSERT INTO games (game_id, name, date) VALUES ({}, {}, {})'
+    template = '''INSERT INTO games (game_id, name, date) VALUES ({}, '{}', '{}')'''
     statements = [template.format(n['id'], n['name'], n['date']) for n in data_loaded['games']]
-
-
+    # checking the list of statements
     print(statements[1])
-print(type(statements))
-
