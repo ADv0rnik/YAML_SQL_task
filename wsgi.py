ea31a1fe728194a5aa1429a1e93aec4dbcb5bc02
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-11-14 15:58:02 +0300

    Updated

--- wsgi.py
+++ wsgi.py
@@ -16,35 +16,42 @@ mysql = MySQL(app)
 @app.route('/toys', methods=['GET'])
 def get_param():
     temp = request.args.get('name')
-    #cur = mysql.connection.cursor()
-    #cur.execute('''INSERT INTO toys (toy_id, name, status, status_updated) VALUES (4, 'car', 'ok', '2018-03-20')''')
-    #toys = cur.execute('''SELECT * FROM toys''')
-    #if toys > 0:
+    # cur = mysql.connection.cursor()
+    # cur.execute('''INSERT INTO toys (toy_id, name, status, status_updated) VALUES (4, 'car', 'ok', '2018-03-20')''')
+    # toys = cur.execute('''SELECT * FROM toys''')
+    # if toys > 0:
         #toys_details = cur.fetchall()
         #print(type(toys_details))
     return temp
-    #render_template('output.html', toys_details=toys_details)
+    # render_template('output.html', toys_details=toys_details)
+
 
 @app.route('/games', methods=['GET'])
 def get_param1():
-    games_date_from = request.args.get('date_from')
-    games_date_to = request.args.get('date_to')
-
-    '''
+    #games_date_from = date_converter(request.args.get('date_from'))
+    #games_date_to = date_converter(request.args.get('date_to'))
+    url = request.url
+    parsed_url = urlparse(url)
+    dic=parse_qs(parsed_url.query)
+    if 'date_from' in dic:
+        games_date_from = date_converter(dic.get('date_from')[0])
+        print(games_date_from)
     cur = mysql.connection.cursor()
-    games = cur.execute(SELECT * FROM toys)
+    '''
+     games = cur.execute(SELECT * FROM games WHERE date >= %s, [games_date_from])
     if games > 0:
         games_details = cur.fetchall()
+        #print(type(toys_details))
     '''
-    print(type(games_date_from))
-    d = date_converter(games_date_from)
-    print(d)
-    return games_date_from
+    print(dic)
+    return request.query_string
         #render_template('output_games.html', games_details=games_details)
+
 
 def date_converter(date:str):
     date_time = datetime.strptime(date, '%Y%m%d').strftime('%Y-%m-%d')
     return date_time
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
